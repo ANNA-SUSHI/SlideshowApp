136de0e54a0091083f95c2b75b22a38cfa0e7a48
ANNA-SUSHI <pequenitaaaaa@gmail.com>
2021-05-01 19:23:30 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Anna T on 2021/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
